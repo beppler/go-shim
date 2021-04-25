a20f93a8bea4dbc91975ef9e95c48068c1f95284
Carlos Alberto Costa Beppler <beppler@gmail.com>
2021-04-24 22:03:11 -0300

    Allow relative paths on command key.

--- main.go
+++ main.go
@@ -31,6 +31,15 @@ func main() {
 		os.Exit(1)
 	}
 
+	if !filepath.IsAbs(cmdPath) {
+		directory, _ := filepath.Split(executable)
+		cmdPath, err = filepath.Abs(filepath.Join(directory, cmdPath))
+		if err != nil {
+			fmt.Println("Error:", err)
+			os.Exit(1)
+		}
+	}
+
 	var cmdArgs []string
 	if config.Section("").Key("args").Value() == "" {
 		cmdArgs = os.Args[1:]
